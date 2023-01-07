5e81f7f6e60164d23db7cc954fc0a77e1caae27e
Lucas Martins <lmartinspg@gmail.com>
2023-01-07 14:34:37 -0300

    Inclusão de novos parâmetros
    Incluídos novos parâmetros para puxar as UTM's do Facebook Ads e também do Google Ads

--- utm.js
+++ utm.js
@@ -1,107 +1,112 @@
 // Recebe os meus parâmetros de UTM
 function getParameter(theParameter) {
-  var params = window.location.search.substr(1).split('&');
-  for (var i = 0; i < params.length; i++) {
-    var p=params[i].split('=');
-     if (p[0] == theParameter) {
-      return decodeURIComponent(p[1]);
+    var params = window.location.search.substr(1).split('&');
+    for (var i = 0; i < params.length; i++) {
+        var p = params[i].split('=');
+        if (p[0] == theParameter) {
+            return decodeURIComponent(p[1]);
+        }
     }
-  }
-  return false;
+    return false;
 }
-url_src = getParameter('utm_source');
-url_mdm = getParameter('utm_medium');
-url_cpn = getParameter('utm_campaign');
+utm_source = getParameter('utm_source');
+utm_medium = getParameter('utm_medium');
+
+// Facebook Ads
+utm_campaign = getParameter('utm_campaign');
+utm_content = getParameter('utm_content');
+campaignid = getParameter('campaignid'); // No Google também
+adsetid = getParameter('adsetid');
+fbclid = getParameter('fbclid');
+adset = getParameter('adset');
+adid = getParameter('adid'); // No Google também
+
+// Google Ads
+utm_term = getParameter('utm_term');
+adgroupid = getParameter('adgroupid');
+targetid = getParameter('targetid');
+
 
 // Recupero dados do meu cookie (se existir) e crio um objeto em JavaScript.
 var pepites = new Object();
 var pate_cookie = Cookies.get('cookie_utms');
 
 // Se pelo menos um parâmetro de URL existir e o cookie não existir
-if((url_src!== false || url_mdm!==false || url_cpn!==false) && (pate_cookie == null || pate_cookie == "" )) {
-  if(url_src!== false){ 
-    pepites["source"] = url_src; 
-  }
-  if(url_mdm!==false){
-    pepites["medium"] = url_mdm; 
-  }
-  if (url_cpn!==false) {
-    pepites["campaign"] = url_cpn;
-  }
-  Cookies.set('cookie_utms', pepites, { expires: 120 } );
-}
+if ((utm_source !== false || utm_medium !== false || campaignid !== false) && (pate_cookie == null || pate_cookie == "")) {
+    if (utm_source !== false) {
+        pepites["source"] = utm_source;
+    }
+    if (utm_medium !== false) {
+        pepites["medium"] = utm_medium;
+    }
+    if (utm_campaign !== false) {
+        pepites["campaign"] = utm_campaign;
+    }
+    if (utm_content !== false) {
+        pepites["content"] = utm_content;
+    }
+    if (campaignid !== false) {
+        pepites["campaignid"] = campaignid;
+    }
+    if (adsetid !== false) {
+        pepites["adsetid"] = adsetid;
+    }
+    if (fbclid !== false) {
+        pepites["fbclid"] = fbclid;
+    }
+    if (adset !== false) {
+        pepites["adset"] = adset;
+    }
+    if (adid !== false) {
+        pepites["adid"] = adid;
+    }
+    if (utm_term !== false) {
+        pepites["term"] = utm_term;
+    }
+    if (adgroupid !== false) {
+        pepites["adgroupid"] = adgroupid;
+    }
+    if (targetid !== false) {
+        pepites["targetid"] = targetid;
+    }
 
-// Caso contrário, se obtivermos pelo menos o parâmetro de URL e o cookie existir
-else if((url_src!== false || url_mdm!==false || url_cpn!==false) && (pate_cookie != null || pate_cookie != "")) {
-  pate_cookie_choco = JSON.parse(pate_cookie);
-
-  if(pate_cookie_choco["source"] != undefined) {
-    if(url_src!== false && pate_cookie_choco["source"].indexOf(url_src) != -1 ){
-      pepites["source"] = pate_cookie_choco["source"]; 
-    }
-    else if(url_src!== false){
-    pepites["source"] = pate_cookie_choco["source"]+"-"+url_src; 
-    }
-    else if ( url_src == false && pate_cookie_choco["source"] != undefined) { 
-    pepites["source"] = pate_cookie_choco["source"]; 
-    }
-  }                            
-  else if ( url_src!== false ) { 
-      pepites["source"] = url_src; 
-  }
-
-  if(pate_cookie_choco["medium"] != undefined) {
-    if(url_mdm!== false && pate_cookie_choco["medium"].indexOf(url_mdm) != -1 ){
-      pepites["medium"] = pate_cookie_choco["medium"];
-    }
-    else if(url_mdm!== false ) { 
-    pepites["medium"] = pate_cookie_choco["medium"]+"-"+url_mdm; 
-    }
-    else if(url_mdm == false){
-    pepites["medium"] = pate_cookie_choco["medium"]; 
-    }
-  }
-  else if(url_mdm!== false){
-    pepites["medium"] = url_mdm; 
-  }
-
-  if(pate_cookie_choco["campaign"] != undefined) {
-    if(url_cpn!== false && pate_cookie_choco["campaign"].indexOf(url_cpn) != -1 ){
-      pepites["campaign"] = pate_cookie_choco["campaign"];
-    }
-    else if(url_cpn!== false) { 
-    pepites["campaign"] = pate_cookie_choco["campaign"]+"-"+url_cpn; 
-    }  
-    else if(url_cpn == false){
-      pepites["campaign"] = pate_cookie_choco["campaign"]; 
-    } 
-  }
-  else if(url_cpn!== false){
-    pepites["campaign"] = url_cpn; 
-  }
-  Cookies.set('cookie_utms', pepites, { expires: 120 } );
+    Cookies.set('cookie_utms', pepites, {
+        expires: 120
+    });
 }
 
 // Recupera os dados do cookie
 var cookie = Cookies.get('cookie_utms');
-if(cookie != undefined){
-  cookie_choco = JSON.parse(cookie);
-  cookie_source = cookie_choco["source"];
-  cookie_medium = cookie_choco["medium"];
-  cookie_campaign = cookie_choco["campaign"];
+if (cookie != undefined) {
+    cookie_choco = JSON.parse(cookie);
+
+    cookie_source = cookie_choco["source"];
+    cookie_medium = cookie_choco["medium"];
+    cookie_campaign = cookie_choco["campaign"];
+
+    cookie_content = cookie_choco["content"];
+    cookie_campaignid = cookie_choco["campaignid"];
+    cookie_adsetid = cookie_choco["adsetid"];
+    cookie_fbclid = cookie_choco["fbclid"];
+    cookie_adset = cookie_choco["adset"];
+    cookie_adid = cookie_choco["adid"];
+    cookie_term = cookie_choco["term"];
+    cookie_adgroupid = cookie_choco["adgroupid"];
+    cookie_adid = cookie_choco["adid"];
+    cookie_targetid = cookie_choco["targetid"];
 }
 
 /**
-* Add um parâmetro em URL ou modificar se já existir
-*/
+ * Add um parâmetro em URL ou modificar se já existir
+ */
 var addParamToUrl = function(url, param, value) {
     param = encodeURIComponent(param);
     var r = "([&?]|&amp;)" + param + "\\b(?:=(?:[^&#]*))*";
     var a = document.createElement('a');
     var regex = new RegExp(r);
-    var str = param + (value ? "=" + encodeURIComponent(value) : ""); 
+    var str = param + (value ? "=" + encodeURIComponent(value) : "");
     a.href = url;
-    var q = a.search.replace(regex, "$1"+str);
+    var q = a.search.replace(regex, "$1" + str);
     if (q === a.search) {
         a.search += (a.search ? "&" : "") + str;
     } else {
@@ -111,18 +116,27 @@ var addParamToUrl = function(url, param, value) {
 }
 
 // Pega todas as urls presentes no site
-urls = document.querySelectorAll('a'); 
+urls = document.querySelectorAll('a');
 
 // Faz uma varredura e verifica se elas sao do WhatsApp
 for (url in urls) {
-	if ( typeof(urls[url].href) != 'undefined' && urls[url].href.includes("https://api.whatsapp.com/") == 1) {
-		
-		// Adiciona os parametros nela
-		var url_modificada = addParamToUrl( urls[url].href, "utm_source", cookie_source);
-		var url_modificada = addParamToUrl( url_modificada, "utm_medium", cookie_medium);
-		var url_modificada = addParamToUrl( url_modificada, "utm_campaign", cookie_campaign);
-		
-		// Faz a troca do href para a nova a url
-		urls[url].href = url_modificada;
-	}
+    if (typeof(urls[url].href) != 'undefined' && urls[url].href.includes("tracking.agenciarazza.com.br/whatsapp") == 1) {
+
+        // Adiciona os parametros nela
+        var url_modificada = addParamToUrl(urls[url].href, "utm_source", cookie_source);
+        var url_modificada = addParamToUrl(url_modificada, "utm_medium", cookie_medium);
+        var url_modificada = addParamToUrl(url_modificada, "utm_campaign", cookie_campaign);
+        var url_modificada = addParamToUrl(url_modificada, "utm_content", cookie_content);
+        var url_modificada = addParamToUrl(url_modificada, "campaignid", cookie_campaignid);
+        var url_modificada = addParamToUrl(url_modificada, "adsetid", cookie_adsetid);
+        var url_modificada = addParamToUrl(url_modificada, "fbclid", cookie_fbclid);
+        var url_modificada = addParamToUrl(url_modificada, "adset", cookie_adset);
+        var url_modificada = addParamToUrl(url_modificada, "adid", cookie_adid);
+        var url_modificada = addParamToUrl(url_modificada, "utm_term", cookie_term);
+        var url_modificada = addParamToUrl(url_modificada, "adgroupid", cookie_adgroupid);
+        var url_modificada = addParamToUrl(url_modificada, "targetid", cookie_targetid);
+
+        // Faz a troca do href para a nova a url
+        urls[url].href = url_modificada;
+    }
 }
